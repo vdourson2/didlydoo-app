99983d07860a15db91c0e5c0ae9ebb0fecd1d065
Lidwine Carême <lidwine.careme@gmail.com>
2023-10-13 16:05:37 +0200

    display modal

--- script/main.js
+++ script/main.js
@@ -1,20 +1,6 @@
-import '/styles/style.scss'
-import { setupCounter } from './components/counter.js'
+import '/styles/style.scss';
+import { addEvent } from './addEvent.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+addEvent();
 
-setupCounter(document.querySelector('#counter'))
+
