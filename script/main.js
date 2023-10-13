be784da57265b93af62415bacb2dc2784ac533cb
Mathias Barbier <mathbarbier07@gmail.com>
2023-10-13 10:21:19 +0200

    update

--- script/main.js
+++ script/main.js
@@ -1,15 +1,11 @@
-import './style.scss'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import '/styles/style.scss'
+import { setupCounter } from './components/counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
