773b6d251d7bbe7270dcb010c99d5b3cef9b5c18
Mathias Barbier <mathbarbier07@gmail.com>
2023-10-13 16:54:52 +0200

    update

--- script/main.js
+++ script/main.js
@@ -3,17 +3,6 @@ import { getEvents } from './getData.js'
 import { postEvents } from './getData.js'
 import { btnAddEvent } from "./components/formRecovery.js";
 
-let testObject = {
-  name: "Test POST db",
-  description: "I try to post a new event on the API",
-  author: "Virginie",
-  dates: [
-    "2023-03-17",
-    "2024-03-18",
-    "2024-03-21",
-    "2024-03-22"
-  ],
-};
 
 postEvents(testObject);
 getEvents();
