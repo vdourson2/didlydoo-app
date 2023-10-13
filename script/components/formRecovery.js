ad86c87aa474617bea0b1117aa542f571f25c15d
tim <timdesmet00@gmail.com>
2023-10-13 15:10:52 +0200

    formRecovery

--- script/components/formRecovery.js
+++ script/components/formRecovery.js
@@ -1,7 +1,7 @@
-const nameEvent = document.getElementById("nameEvent");
-const descriptionEvent = document.getElementById("descriptionEvent");
+const nameEvent = document.getElementById("eventTitle");
+const descriptionEvent = document.getElementById("description");
 const authorEvent = document.getElementById("authorEvent");
-const dateEvent = document.getElementById("dateEvent");
+const dateEvent = document.getElementById("eventDate");
 const btnAddEvent = document.getElementById("btnAddEvent");
 
 btnAddEvent.addEventListener("click", () => {
@@ -9,8 +9,14 @@ btnAddEvent.addEventListener("click", () => {
     name: nameEvent.value,
     description: descriptionEvent.value,
     author: authorEvent.value,
-    date: dateEvent.value,
+    date: [],
   };
+
+  const datestring = dateEvent.value.split(",");
+  datestring.forEach((date) => {
+    event.date.push(new Date(datestring.trim()));
+  });
+
   if (event.name.length > 256 || event.description.length > 256) {
     alert(
       "Le nom et la description de l'évènement ne doivent pas dépasser 256 caractères"
