1a6c85f11d8829b2bd8e776f7ec91ad1c212a9d6
Virginie Dourson <virginiedourson@gmail.com>
2023-10-13 16:47:58 +0200

    Change getEvents to mangage just one event

--- script/getData.js
+++ script/getData.js
@@ -1,7 +1,9 @@
-//Cette fonction réculpère la liste de tous les events stockés dans l'API
-export async function getEvents(){
+//Si aucun argument n'est passé à la fonction, cette fonction récupère la liste de 
+//tous les events stockés dans l'API et lance l'affichage des données
+//Si un id est communiqué en argument, cette fonction renvoie uniquement l'event ayant l'id indiqué
+export async function getEvents(id = ""){
     try{
-        let promise = await fetch('http://localhost:3000/api/events/');
+        let promise = await fetch(`http://localhost:3000/api/events/${id}`);
         let eventList = await promise.json();
         console.log(eventList);
         //Appeler la fonction display de Mathias
@@ -11,6 +13,9 @@ export async function getEvents(){
     }
 }
 
+
+//Cette fonction prend un objet Javascript contenant un nouvel event 
+//et l'ajoute à la base de données. 
 export async function postEvents(eventObject){
     try{
         console.log(JSON.stringify(eventObject));
@@ -22,9 +27,10 @@ export async function postEvents(eventObject){
             },
             body: JSON.stringify(eventObject),
         });
-    
     }
     catch(error){
-
+        console.log("Impossible d'envoyer les données", error);
     }
 }
+
+//export async function 
