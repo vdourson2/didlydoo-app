a43b9df6d4dbd3689abf08cef1fc31b0e07799f5
Lidwine Carême <lidwine.careme@gmail.com>
2023-10-16 14:26:24 +0200

    renommer dossier

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 import '/styles/style.scss';
-import { getEvents } from './getData.js'
-import { postEvents } from './getData.js'
+import { getEvents } from './fetchData.js'
+import { postEvents } from './fetchData.js'
 import { addEvent } from './addEvent.js'
 import { date } from './newDates.js';
 import { btnAddEvent } from "./components/formRecovery.js"
