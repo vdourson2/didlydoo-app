f6e7fa036144d6254a96d8334154a2d5b049fd86
tim <timdesmet00@gmail.com>
2023-10-16 15:30:22 +0200

    lilalou

--- script/main.js
+++ script/main.js
@@ -1,14 +1,14 @@
 import "/styles/style.scss";
-import { getEvents } from "./fetchData.js";
+import { getData } from "./fetchData.js";
 import { postEvents } from "./fetchData.js";
 //import { add } from "./addEvent.js";
 //import { send } from "./addEvent.js";
-import { superBtn } from "./components/formRecovery.js";
 import { addEvent } from "./addEvent.js";
 import { date } from "./newDates.js";
+import { superBtn } from "./components/formRecovery.js";
 
+getData();
 postEvents();
-getEvents();
 addEvent();
 //add();
 //send();
