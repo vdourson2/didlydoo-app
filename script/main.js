9245f185ffd9f8ae4eca9b536f8df11bc574742c
tim <timdesmet00@gmail.com>
2023-10-16 14:33:38 +0200

    la la la ala

--- script/main.js
+++ script/main.js
@@ -4,9 +4,8 @@ import { postEvents } from "./getData.js";
 //import { add } from "./addEvent.js";
 //import { send } from "./addEvent.js";
 import { superBtn } from "./components/formRecovery.js";
-import { addEvent } from './addEvent.js'
-import { date } from './newDates.js';
-
+import { addEvent } from "./addEvent.js";
+import { date } from "./newDates.js";
 
 postEvents();
 getEvents();
@@ -15,6 +14,3 @@ addEvent();
 //send();
 date();
 superBtn();
-
-
-
