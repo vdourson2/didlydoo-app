5b7dadcc2ae02746492c770b829718a3354e1bd1
Virginie Dourson <virginiedourson@gmail.com>
2023-10-16 16:28:40 +0200

    Add PATCH and POST dates

--- script/main.js
+++ script/main.js
@@ -3,19 +3,35 @@ import { setupCounter } from './components/counter.js'
 import { getData } from './fetchData.js'
 import { postEvents } from './fetchData.js'
 import { deleteEvent } from './fetchData.js'
+import { patchEvents } from './fetchData.js'
+import { patchAttend } from './fetchData.js'
+import { postDates } from './fetchData.js'
 
-// let testObject = {
-//   name: "Test POST db",
-//   description: "I try to post a new event on the API",
-//   author: "Virginie",
-//   dates: [
-//     "2023-03-17",
-//     "2024-03-18",
-//     "2024-03-21",
-//     "2024-03-22"
-//   ],
+// let testPatchObject = {
+//   name: "Test PATCH db",
+// };
+
+// let testPatchAttend = {
+//   name : "Michou",
+//   dates : [
+//     {
+//       "date": "2022-03-17",
+//       "available": true
+//     },
+//     {
+//       "date": "2022-03-18",
+//       "available": false
+//     },
+//     {
+//       "date": "2022-03-21",
+//       "available": true
+//     }
+//   ]
 // };
 
 //postEvents(testObject);
 //getData();
 //deleteEvent("f25013cfb2c9");
+//patchEvents("4869d249ef3c",testPatchObject);
+//patchAttend("f5b6564b5dc4",testPatchAttend);
+// postDates("f5b6564b5dc4",{dates : ["2023-03-21","2023-03-22"]});
